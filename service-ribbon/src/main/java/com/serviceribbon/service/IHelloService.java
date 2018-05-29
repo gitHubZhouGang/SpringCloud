0e1c17ab22eda68a72999aaa5e4439a04830024b
Chio <2284957575@qq.com>
2018-05-29 16:46:36 +0800

    ribbon+resttemplate 加Hystrix熔断器监视器

--- service-ribbon/src/main/java/com/serviceribbon/service/IHelloService.java
+++ service-ribbon/src/main/java/com/serviceribbon/service/IHelloService.java
@@ -1,8 +1,10 @@
 package com.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 
 public interface IHelloService {
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name);
 }
