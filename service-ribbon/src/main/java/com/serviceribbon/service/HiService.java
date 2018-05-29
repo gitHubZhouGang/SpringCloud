e21a9523790704c43a16e1f2a17432ac60003cd9
Chio <2284957575@qq.com>
2018-05-29 11:14:58 +0800

    ribbon+resttemplate 加Hystrix熔断器

--- service-ribbon/src/main/java/com/serviceribbon/service/HiService.java
+++ service-ribbon/src/main/java/com/serviceribbon/service/HiService.java
@@ -1,5 +1,7 @@
 package com.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +11,14 @@ public class HiService implements IHelloService {
 
     @Autowired
     RestTemplate restTemplate;
+
     @Override
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-EUREKA/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name){
+        return "hi,"+name+",sorry,it is error! Pls retry later";
     }
 }
--- service-ribbon/src/main/java/com/serviceribbon/service/IHelloService.java
+++ service-ribbon/src/main/java/com/serviceribbon/service/IHelloService.java
@@ -1,5 +1,7 @@
 package com.serviceribbon.service;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+
 public interface IHelloService {
 
     public String hiService(String name);
